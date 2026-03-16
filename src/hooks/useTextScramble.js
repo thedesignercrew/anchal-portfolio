86e3f1906ee053c15076327be85b752c3bbfbc5b
nitesh <niteshatqtos@gmail.com>
2026-03-16 15:06:17 +0530

    remove scramble text

--- src/hooks/useTextScramble.js
+++ src/hooks/useTextScramble.js
@@ -10,21 +10,6 @@ const useTextScramble = (text, trigger) => {
       setDisplay(text);
       return;
     }
-    let iteration = 0;
-    const interval = setInterval(() => {
-      setDisplay(
-        text
-          .split("")
-          .map((letter, index) => {
-            if (index < iteration) return text[index];
-            return chars[Math.floor(Math.random() * chars.length)];
-          })
-          .join(""),
-      );
-      if (iteration >= text.length) clearInterval(interval);
-      iteration += 1 / 2;
-    }, 30);
-    return () => clearInterval(interval);
   }, [trigger, text]);
 
   return display;
